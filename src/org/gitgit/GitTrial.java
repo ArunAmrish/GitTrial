ee06f99e5f2805921b65932b975f7c2ca842540a
ArunAmrish <amrish.sep12@gmail.com>
2022-09-24 15:47:25 -0700

    Hai there

---
+++ src/org/gitgit/GitTrial.java
@@ -0,0 +1,30 @@
+package org.gitgit;
+
+public class GitTrial {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+		System.out.println(6);
+		System.out.println(7);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
